a437d523bd406b626514d548a8445264716f76c5
losier <arv95464@gmail.com>
2024-06-27 21:01:20 +0530

    dixed some typos and imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * as arithmetic from "./arithmetic";
+export * as advancedMath from "./advancedMath";
+export * as numberClassifications from "./numberClassification";
 
 // types
 export * from "./types/arithmeticTypes";
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,26 +1,11 @@
 import { defineConfig } from "tsup";
-import { readdirSync } from "fs";
-import { join } from "path";
-
-function getDirectories(path: string): string[] {
-  return readdirSync(path, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-}
-
-function getEntries(): string[] {
-  const modulesDir = join(__dirname, "src");
-  const moduleNames = getDirectories(modulesDir);
-  const entries: string[] = [];
-  moduleNames.forEach((moduleName) => {
-    const modulePath = join(modulesDir, moduleName, "index.ts");
-    entries.push(modulePath);
-  });
-  return entries;
-}
-
 export default defineConfig({
-  entry: [...getEntries()],
+  entry: [
+    "src/index.ts",
+    "src/arithmetic/index.ts",
+    "src/advancedMath/index.ts",
+    "src/numberClassification/index.ts",
+  ],
   format: ["esm", "cjs"],
   target: "es2022",
   sourcemap: true,
