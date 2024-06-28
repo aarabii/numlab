e1e9a59922d01069a1ed218570883c34108e504d
losier <arv95464@gmail.com>
2024-06-28 09:57:15 +0530

    Refactor arithmetic functions to use destructured arguments

--- src/advancedMath/functions.ts
+++ src/advancedMath/functions.ts
@@ -1,16 +1,16 @@
 import * as numTypes from "../types/advancedMath.types";
 
-export function factorial({ n }: numTypes.FactorialFunction): number {
+export function factorial({ n }: numTypes.FactorialProps): number {
   if (n === 0) return 1;
   return n * factorial({ n: n - 1 });
 }
 
-export function fibonacci({ n }: numTypes.FibonacciFunction): number {
+export function fibonacci({ n }: numTypes.FibonacciProps): number {
   if (n <= 1) return n;
   return fibonacci({ n: n - 1 }) + fibonacci({ n: n - 2 });
 }
 
-export function isPrime({ n }: numTypes.IsPrimeFunction): boolean {
+export function isPrime({ n }: numTypes.IsPrimeProps): boolean {
   if (n <= 1) return false;
   if (n <= 3) return true;
   if (n % 2 === 0 || n % 3 === 0) return false;
@@ -22,19 +22,19 @@ export function isPrime({ n }: numTypes.IsPrimeFunction): boolean {
   return true;
 }
 
-export function isComposite({ n }: numTypes.IsCompositeFunction): boolean {
+export function isComposite({ n }: numTypes.IsCompositeProps): boolean {
   return !isPrime({ n });
 }
 
-export function gcd({ a, b }: numTypes.GcdFunction): number {
+export function gcd({ a, b }: numTypes.GcdProps): number {
   return b === 0 ? a : gcd({ a: b, b: a % b });
 }
 
-export function lcm({ a, b }: numTypes.LcmFunction): number {
+export function lcm({ a, b }: numTypes.LcmProps): number {
   return Math.abs(a * b) / gcd({ a, b });
 }
 
-export function primeFactors({ n }: numTypes.PrimeFactorsFunction): number[] {
+export function primeFactors({ n }: numTypes.PrimeFactorsProps): number[] {
   const factors = [];
   let num = n;
   for (let i = 2; i <= num; i++) {
@@ -46,12 +46,12 @@ export function primeFactors({ n }: numTypes.PrimeFactorsFunction): number[] {
   return factors;
 }
 
-export function binomial({ n, k }: numTypes.BinomialFunction): number {
+export function binomial({ n, k }: numTypes.BinomialProps): number {
   if (k === 0 || k === n) return 1;
   return binomial({ n: n - 1, k: k - 1 }) + binomial({ n: n - 1, k });
 }
 
-export function hermite({ n, x }: numTypes.HermiteFunction): number {
+export function hermite({ n, x }: numTypes.HermiteProps): number {
   if (n === 0) return 1;
   if (n === 1) return 2 * x;
   return (
@@ -59,7 +59,7 @@ export function hermite({ n, x }: numTypes.HermiteFunction): number {
   );
 }
 
-export function laguerre({ n, x }: numTypes.LaguerreFunction): number {
+export function laguerre({ n, x }: numTypes.LaguerreProps): number {
   if (n === 0) return 1;
   if (n === 1) return 1 - x;
   return (
@@ -68,7 +68,7 @@ export function laguerre({ n, x }: numTypes.LaguerreFunction): number {
   );
 }
 
-export function legendre({ n, x }: numTypes.LegendreFunction): number {
+export function legendre({ n, x }: numTypes.LegendreProps): number {
   if (n === 0) return 1;
   if (n === 1) return x;
   return (
@@ -77,19 +77,19 @@ export function legendre({ n, x }: numTypes.LegendreFunction): number {
   );
 }
 
-export function chebyshev({ n, x }: numTypes.ChebyshevFunction): number {
+export function chebyshev({ n, x }: numTypes.ChebyshevProps): number {
   if (n === 0) return 1;
   if (n === 1) return x;
   return 2 * x * chebyshev({ n: n - 1, x }) - chebyshev({ n: n - 2, x });
 }
 
-export function bessel({ n, x }: numTypes.BesselFunction): number {
+export function bessel({ n, x }: numTypes.BesselProps): number {
   if (n === 0) return bessel({ n: 0, x });
   if (n === 1) return bessel({ n: 0, x });
   return ((2 * n - 1) / x) * bessel({ n: n - 1, x }) - bessel({ n: n - 2, x });
 }
 
-export function gamma({ n }: numTypes.GammaFunction): number {
+export function gamma({ n }: numTypes.GammaProps): number {
   if (n === 0) {
     throw new Error("Gamma function is undefined for zero.");
   }
@@ -97,11 +97,11 @@ export function gamma({ n }: numTypes.GammaFunction): number {
   return (n - 1) * gamma({ n: n - 1 });
 }
 
-export function beta({ x, y }: numTypes.BetaFunction): number {
+export function beta({ x, y }: numTypes.BetaProps): number {
   return (gamma({ n: x }) * gamma({ n: y })) / gamma({ n: x + y });
 }
 
-export function zeta({ n }: numTypes.ZetaFunction): number {
+export function zeta({ n }: numTypes.ZetaProps): number {
   let sum = 0;
   for (let i = 1; i < 10000; i++) {
     sum += 1 / i ** n;
